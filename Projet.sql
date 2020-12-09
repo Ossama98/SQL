6845ab86fa7626776962b522be1f80a371e210f7
Ossama <ossama1998@windowslive.com>
2020-12-09 19:04:29 +0100

    Ajout des méthodes d'ordre dans les types + ajout quelques méthodes applicatives

--- Projet.sql
+++ Projet.sql
@@ -1,5 +1,6 @@
+
 drop type   AUTEUR_T force;
-drop type   TAB_REF_AUTEURS ;
+drop type   TAB_REF_AUTEURS force ;
 drop type   CATALOGUE_T force;
 drop type   EXEMPLAIRE_T force;
 drop type   TABEXEMPLAIRES_T force;
@@ -11,10 +12,11 @@ drop type   EMPRUNT_T force;
 drop type   RESEAU_T force;
 
 
+
 CREATE OR REPLACE TYPE AUTEUR_T
 /
 
-CREATE OR REPLACE TYPE TAB_REF_AUTEURS AS TABLE OF REF AUTEUR_T ;
+CREATE OR REPLACE TYPE TAB_REF_AUTEURS AS TABLE OF REF AUTEUR_T
 /
 
 CREATE OR REPLACE TYPE CATALOGUE_T
@@ -26,17 +28,19 @@ TITRE               VARCHAR2(20),
 ANNEE_EDITION       DATE,
 MAISON_EDITION      VARCHAR2(20),
 REF_AUTEURS         TAB_REF_AUTEURS,
-DESCRIPTION         CLOB
+DESCRIPTION         CLOB,
+MAP MEMBER FUNCTION compAnneeEDITION return DATE  -- comparer avec ANNEE_EDITION décroissant
 );
 /
 
 CREATE OR REPLACE TYPE EXEMPLAIRE_T AS OBJECT(
 ID                  NUMBER(4),
-REF_CATALOGUE       REF CATALOGUE_T
+REF_CATALOGUE       REF CATALOGUE_T,
+MAP MEMBER FUNCTION compID return NUMBER -- comparer avec ID croissant
 );
 /
 
-CREATE OR REPLACE TYPE TABEXEMPLAIRES_T AS VARRAY(4) OF EXEMPLAIRE_T ;
+CREATE OR REPLACE TYPE TABEXEMPLAIRES_T AS TABLE OF EXEMPLAIRE_T
 /
 
 CREATE OR REPLACE TYPE BIBLIOTHEQUE_T AS OBJECT(
@@ -44,7 +48,9 @@ ID                          NUMBER(4),
 REGION                      VARCHAR(20),
 EXEMPLAIRES                 TABEXEMPLAIRES_T,
 ADDRESSE                    VARCHAR(20),
-VILLE                       VARCHAR(20)
+VILLE                       VARCHAR(20),
+MAP MEMBER FUNCTION compRegionVille return VARCHAR2, -- comparer avec région||ville
+MEMBER PROCEDURE ajoutExemplaire(exemplaire in EXEMPLAIRE_T)
 );
 /
 
@@ -55,9 +61,12 @@ CREATE OR REPLACE TYPE EMPRUNT_T AS OBJECT(
 ID                      NUMBER(4),
 REF_ADHERENT            REF ADHERENT_T,
 DATE_START              DATE ,
-DATE_END                DATE ,
-DATE_RETOUR             DATE ,
-REF_EXEMPLAIRE          REF EXEMPLAIRE_T
+DATE_END                DATE , --date de fin de l'emprunt
+DATE_RETOUR             DATE , --date effective de retour
+REF_EXEMPLAIRE          REF EXEMPLAIRE_T,
+MAP MEMBER FUNCTION compDateEnd return DATE, -- comparer avec DATE_END croissant
+MEMBER FUNCTION prolongerDateEnd(nbJour in NUMBER) return DATE,
+MEMBER PROCEDURE testRetard
 );
 /
 
@@ -75,9 +84,10 @@ REF_BIBLIOTHEQUE           TAB_REF_BIBLIOTHEQUE,
 ADRESSE                    VARCHAR2(20),
 PHONE                      VARCHAR2(20),
 EMAIL                      VARCHAR2(20),
-ANNEE_DE_NAISSANCE         DATE,
+DATE_DE_NAISSANCE          DATE,
 DATE_D_ADHESION            DATE,
-VILLE                      VARCHAR2(20)
+VILLE                      VARCHAR2(20),
+MAP MEMBER FUNCTION compNomPrenomsVille return VARCHAR2 -- comparer avec NOM||PRENOM||VILLE
 );
 /
 
@@ -89,7 +99,8 @@ DATE_DE_NAISSANCE           DATE,
 DATE_DE_DECES               DATE,
 NATIONALITE                 VARCHAR(20),
 VILLE                       VARCHAR(20),
-BIOGRAPHIE                  CLOB
+BIOGRAPHIE                  CLOB,
+MAP MEMBER FUNCTION compNomPrenomsNaissance return VARCHAR2 -- comparer avec NOM||PRENOM||DATE_DE_NAISSANCE
 );
 /
 
@@ -100,7 +111,8 @@ CREATE OR REPLACE TYPE TAB_REF_BIBLIOTHEQUE_T AS TABLE OF REF BIBLIOTHEQUE_T
 CREATE OR REPLACE TYPE RESEAU_T AS OBJECT(
 ID                          NUMBER(4),
 NOM                         VARCHAR(20),
-BIBLIOTHEQUES               TAB_REF_BIBLIOTHEQUE_T
+BIBLIOTHEQUES               TAB_REF_BIBLIOTHEQUE_T,
+MAP MEMBER FUNCTION compNom return VARCHAR2 -- comparer avec NOM croissant
 );
 /
 
